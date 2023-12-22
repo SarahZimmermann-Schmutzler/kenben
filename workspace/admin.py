1d6687abb3655cf4051bfa26d21762f8fd31e068
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-22 12:38:25 +0100

    model subtask

--- workspace/admin.py
+++ workspace/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Boards, Tickets
+from .models import Boards, Tickets, Subtasks
 
 # Register your models here.
 admin.site.register(Boards)
 admin.site.register(Tickets)
+admin.site.register(Subtasks)
--- workspace/models.py
+++ workspace/models.py
@@ -6,16 +6,21 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Boards(models.Model):
+
+
+class Subtasks(models.Model):
     title = models.CharField(max_length=100)
-    creator = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+    description = models.CharField(max_length=300, default=None)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, default=None)
     created_at = models.DateField(default=datetime.date.today)
+    checked = models.BooleanField(default=False)
+    assigned = models.ManyToManyField(User, related_name='members')
+    due_date = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return f'{self.id} {self.title}'
+    
+    # funktion wie lange bis due_date
 
 
 class Tickets(models.Model):
@@ -41,7 +46,7 @@ class Tickets(models.Model):
     }
 
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=300, default=None)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
     created_at = models.DateField(default=datetime.date.today)
     checked = models.BooleanField(default=False)
@@ -49,8 +54,18 @@ class Tickets(models.Model):
     priority = models.CharField(max_length=20, choices=priority_choices, default=low)
     status = models.CharField(max_length=20, choices=status_choices, default=todo)
     due_date = models.DateField(default=datetime.date.today)
+    subtasks = models.ManyToManyField(Subtasks, related_name='subtasks', default=None)
 
     def __str__(self):
         return f'{self.id} {self.title}'
     
     # funktion wie lange bis due_date
+
+class Boards(models.Model):
+    title = models.CharField(max_length=100)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    created_at = models.DateField(default=datetime.date.today)
+    tickets = models.ManyToManyField(Tickets, related_name='tickets')
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
