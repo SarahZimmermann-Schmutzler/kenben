19ebb3d0cbd1e9ffd7896834022d0c832340cc8c
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-12-28 10:39:20 +0100

    django-cors-headers

--- kenben/settings.py
+++ kenben/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1'
     ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'kenben.urls'
